f511da698fe0512a534f090bdec63481f9f6aee7
hydai <hydai@users.noreply.github.com>
2022-03-28 10:05:41 +0000

    deploy: 7e791fbc1f7c8023b826526454b1638298654857

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["wasmedge_sys"] = {"name":"","dirs":[{"name":"instance","files":["function.rs","global.rs","memory.rs","mod.rs","module.rs","table.rs"]}],"files":["compiler.rs","config.rs","error.rs","executor.rs","import_obj.rs","io.rs","lib.rs","loader.rs","module.rs","statistics.rs","store.rs","types.rs","utils.rs","validator.rs","vm.rs"]};
+createSourceSidebar();
